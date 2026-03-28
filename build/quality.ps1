14c16cefd3103ee63fcef2480f4bc6322a918c26
Garume <80187947+Garume@users.noreply.github.com>
2026-03-28 09:28:12 +0900

    Build before format in quality checks

--- build/quality.ps1
+++ build/quality.ps1
@@ -14,17 +14,17 @@ if (-not $SkipRestore) {
     }
 }
 
-& (Join-Path $PSScriptRoot 'format.ps1') -Solution $Solution -NoRestore
-if ($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
-}
-
 & (Join-Path $PSScriptRoot 'build.ps1') -Solution $Solution -DalamudHome $DalamudHome -NoRestore
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-& (Join-Path $PSScriptRoot 'test.ps1') -Solution $Solution -DalamudHome $DalamudHome
+& (Join-Path $PSScriptRoot 'format.ps1') -Solution $Solution -NoRestore
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+& (Join-Path $PSScriptRoot 'test.ps1') -Solution $Solution -DalamudHome $DalamudHome -NoBuild
 if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
